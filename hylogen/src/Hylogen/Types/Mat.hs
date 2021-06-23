754442f0b23856e442c42298373c441a33325de4
Takumi Crary <tcrary@gmail.com>
2021-06-23 22:37:44 +0900

    Add frac and floating instances for Mat

--- hylogen/src/Hylogen/Types/Mat.hs
+++ hylogen/src/Hylogen/Types/Mat.hs
@@ -109,6 +109,30 @@ instance (Mattable n m) => Num (Mat n m) where
     negate = op1 "-"
     fromInteger x = copyM . uniform . show $ (fromInteger x :: Float)
 
+instance (Mattable n m) => Fractional (Mat n m) where
+    (/) = op2' "/"
+    fromRational x = copyM . uniform . show $ (fromRational x :: Float)
+
+instance (Mattable n m) => Floating (Mat n m) where
+    -- pi = copy $ uniform "pi"
+    pi = copyM $ uniform "3.141592653589793238462643383"
+    exp = op1pre "exp"
+    log = op1pre "log"
+    sqrt = op1pre "sqrt"
+    (**) = op2pre' "pow"
+    sin = op1pre "sin"
+    cos = op1pre "cos"
+    tan = op1pre "tan"
+    asin = op1pre "asin"
+    acos = op1pre "acos"
+    atan = op1pre "atan"
+    sinh x = (exp x - exp (negate x)) / 2
+    cosh x = (exp x + exp (negate x))/2
+    tanh x = sinh x / cosh x
+    asinh x = log $ x + sqrt(x**2 + 1)
+    acosh x = log $ x + sqrt(x**2 - 1)
+    atanh x = 0.5 * log ((1 + x)/(1 - x))
+
 --Darn looks like we need to figure this out some more
 --instance {-#OVERLAPPABLE#-} (a ~ Mattable n n, b ~ Veccable n) => Num (Vec n) where
 
